0c44881b16874f3ade368b7bc5200f45e43a8ccb
Andre0x1 <andrel.silva3357@gmail.com>
2020-09-14 23:33:58 -0300

    Segundo commit

--- CSV_teste.py
+++ CSV_teste.py
@@ -1,4 +1,11 @@
 import csv
 
 
-print("Hello world")
+with open('Teste.csv', 'w' , newline= '') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"])
+    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
+    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
+    writer.writerow([3, "Guido van Rossum", "Python Programming"])
+    writer.writerow([4, "andre", "Python Programming"] )
+
